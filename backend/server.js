92f1ecb1ced6bff9aa1b297a6be33a2e4378e726
Iman1121 <imanullah1112@gmail.com>
2023-05-28 21:41:31 -0400

    started express

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const path = require("path");
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.json({ mssg: "App" });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Server started on port", process.env.PORT);
+});
+process.env;
