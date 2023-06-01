8d9482f18def0c0aa2ab10b679af786336a7a867
Iman1121 <imanullah1112@gmail.com>
2023-06-01 10:14:44 -0400

    schema finished

--- backend/model/courseModel.js
+++ backend/model/courseModel.js
@@ -4,12 +4,7 @@ const Schema = mongoose.Schema;
 
 const courseSchema = new Schema(
   {
-    id: {
-      type: String,
-    },
-    registration_term: {
-      type: String,
-    },
+    registration_term: {},
     faculty: {},
     course_code: {},
     section: {},
@@ -17,6 +12,26 @@ const courseSchema = new Schema(
     crn: {},
     title: {},
     course_description: {},
+    precludes_string: {},
+    precludes_courses: {},
+    course_credit_value: {},
+    schedule_type: {},
+    status: {},
+    section_information: {},
+    year_in_program: {},
+    level_restriction: {},
+    degree_restriction: {},
+    major_restrictions: {},
+    program_restriction: {},
+    department_restriction: {},
+    faculty_restrictions: {},
+    start_date: {},
+    end_time: {},
+    building: {},
+    room: {},
+    schedule: {},
+    instructor: {},
+    instructor_status: {},
   },
   {
     timestamps: true,
