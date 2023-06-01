7b546e4fa9a9a392bb17e31823b0d3f9348abbe8
Brandon Le <brandonle2@outlook.com>
2023-06-01 01:17:43 -0400

    added CRUD functions

--- backend/routes/courses.js
+++ backend/routes/courses.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { getCourse, getCourses } = require("../controllers/courseController");
+const { getCourse, getAllCourses,createCourse,deleteCourse,updateCourse } = require("../controllers/courseController");
 
 //get specified course
 router.get("/:id", getCourse);
-router.get("/", getCourses);
-
+router.get("/", getAllCourses);
+router.post("",createCourse)
+router.delete("/:id", deleteCourse)
+router.patch("/:id", updateCourse)
 module.exports = router;
